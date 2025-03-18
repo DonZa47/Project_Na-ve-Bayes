c6f415282f518e6574e4083ee46314f90ea18456
DonZa47 <nudbun73@gmail.com>
2025-03-18 11:27:53 +0700

    Update ืNaivebayes.py

--- ืNaivebayes.py
+++ ืNaivebayes.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 
 # โหลดชุดข้อมูล Iris
-Heart= pd.read('./data/heart2.csv')
+Heart= pd_read('./data/heart2.csv')
 x =Heart.drop(columns=['HeartDisease'])
 y =Heart.HeartDisease
 HeartDisease = y
